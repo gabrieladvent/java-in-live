efa9fad885ae678850c63d13568b02577e03c8a1
gabrieladvent <bie.ritan112@gmail.com>
2023-06-20 15:50:08 +0700

    stokastik laporan

--- stosk/stosk/src/ProgramFix/bloodPresureprediction.java
+++ stosk/stosk/src/ProgramFix/bloodPresureprediction.java
@@ -2,19 +2,160 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ProgramFix;
 
 /**
  *
  * @author ASUS
  */
+package ProgramFix;
+
 import java.text.DecimalFormat;
 import java.util.Scanner;
+
 public class bloodPresureprediction {
-    
+
+    // Fungsi untuk menghitung matriks transisi perubahan
+    public static int[][] calculateTransitionMatrix(int[] bloodPressures) {
+        int[][] countMatrix = new int[4][4];
+        for (int i = 0; i < bloodPressures.length - 1; i++) {
+            int currentState = getState(bloodPressures[i]);
+            int nextState = getState(bloodPressures[i + 1]);
+            countMatrix[currentState][nextState]++;
+        }
+        return countMatrix;
+    }
+
+    // Fungsi untuk menghitung probabilitas matriks transisi
+    public static double[][] calculateProbabilityMatrix(int[][] transitionMatrix) {
+        int n = transitionMatrix.length;
+        double[][] probabilityMatrix = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            int totalTransitions = 0;
+            for (int j = 0; j < n; j++) {
+                totalTransitions += transitionMatrix[i][j];
+            }
+            for (int j = 0; j < n; j++) {
+                if (totalTransitions != 0) {
+                    probabilityMatrix[i][j] = (double) transitionMatrix[i][j] / totalTransitions;
+                }
+            }
+        }
+        return probabilityMatrix;
+    }
+
+    // Fungsi untuk menghitung matriks distribusi stabil (steady state)
+    public static double[] calculateSteadyState(double[][] probabilityMatrix) {
+        int n = probabilityMatrix.length;
+        double[] steadyState = new double[n];
+        for (int i = 0; i < n; i++) {
+            double sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += probabilityMatrix[j][i];
+            }
+            steadyState[i] = sum / n;
+        }
+        return steadyState;
+    }
+
+    // Fungsi utilitas untuk mendapatkan indeks keadaan dari tekanan darah
+    public static int getState(int bloodPressure) {
+        if (bloodPressure < 120) {
+            return 0; // Normal
+        } else if (bloodPressure >= 120 && bloodPressure <= 139) {
+            return 1; // Pre-Hipertensi
+        } else if (bloodPressure >= 140 && bloodPressure <= 159) {
+            return 2; // Hipertensi Stage 1
+        } else {
+            return 3; // Hipertensi Stage 2
+        }
+    }
+
+    // Fungsi untuk menampilkan matriks
+    public static void displayMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Fungsi untuk menampilkan matriks dengan tipe data double
+    public static void displayMatrix(double[][] matrix) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(decimalFormat.format(matrix[i][j]) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Fungsi untuk menampilkan vektor dengan tipe data double
+    public static void displayVector(double[] vector) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        String[] kategori = {"Normal", "Pre-Hipertensi", "Hipertensi Stage 1", "Hipertensi Stage 2"};
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("Status " + i + " (" + kategori[i] + ") memiliki probabilitas stabil sebesar " +
+                    decimalFormat.format(vector[i]) + " atau " + decimalFormat.format(vector[i] * 100) + "%.");
+        }
+    }
+
+    // Fungsi untuk menampilkan vektor dengan tipe data double dalam bentuk persen
+    public static void displayVectorPercentage(double[] vector, String[] kategori) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00%");
+        double sum = 0.0;
+        for (double value : vector) {
+            sum += value;
+        }
+        for (int i = 0; i < vector.length; i++) {
+            double percentage = (vector[i] / sum) * 100;
+            System.out.println(kategori[i] + ": " + decimalFormat.format(percentage / 100));
+        }
+    }
+
+    // Fungsi untuk menampilkan elemen dengan persentase terbesar dari vektor prediksi
+    public static void displayLargestProbability(double[] vector, String[] kategori) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00%");
+        int maxIndex = 0;
+        double maxProbability = vector[0];
+        for (int i = 1; i < vector.length; i++) {
+            if (vector[i] > maxProbability) {
+                maxProbability = vector[i];
+                maxIndex = i;
+            }
+        }
+        System.out.println("\nKemungkinan terbesar:");
+        System.out.println(kategori[maxIndex]);
+    }
+
+    // Fungsi untuk menghitung prediksi jangka pendek atau jangka panjang
+    public static double[] calculatePrediction(double[] steadyState, double[][] probabilityMatrix, int days) {
+        int n = steadyState.length;
+        double[] prediction = new double[n];
+        for (int i = 0; i < n; i++) {
+            double sum = steadyState[i];
+            for (int j = 0; j < n; j++) {
+                sum += probabilityMatrix[i][j] * steadyState[j] * days;
+            }
+            prediction[i] = sum;
+        }
+        // Normalisasi prediksi menjadi maksimal 100%
+        double maxPrediction = prediction[0];
+        for (int i = 1; i < n; i++) {
+            if (prediction[i] > maxPrediction) {
+                maxPrediction = prediction[i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            prediction[i] = (prediction[i] / maxPrediction) * 100;
+        }
+        return prediction;
+    }
 
     public static void main(String[] args) {
         int[] bloodPressures;
+        String[] kategori = {"Normal", "Pre-Hipertensi", "Hipertensi Stage 1", "Hipertensi Stage 2"};
         Scanner scanner = new Scanner(System.in);
 
         // Memilih jenis data (bulanan atau mingguan)
@@ -45,155 +186,33 @@ public class bloodPresureprediction {
         System.out.println("\nMatriks Transisi Perubahan:");
         displayMatrix(transitionMatrix);
 
+        // Menghitung probabilitas matriks transisi
+        double[][] probabilityMatrix = calculateProbabilityMatrix(transitionMatrix);
+
+        // Menampilkan probabilitas matriks transisi
+        System.out.println("\nProbabilitas Matriks Transisi:");
+        displayMatrix(probabilityMatrix);
+
         // Menghitung matriks distribusi stabil
-        double[] steadyState = calculateSteadyState(transitionMatrix);
+        double[] steadyState = calculateSteadyState(probabilityMatrix);
 
         // Menampilkan matriks distribusi stabil
         System.out.println("\nMatriks Distribusi Stabil:");
         displayVector(steadyState);
 
-        // Prediksi tekanan darah jangka pendek
-        int normalCount = 0;
-        int preHypertensionCount = 0;
-        int hypertension1Count = 0;
-        int hypertension2Count = 0;
-
-        for (int bloodPressure : bloodPressures) {
-            String prediction = calculatePrediction(bloodPressure, steadyState);
-
-            switch (prediction) {
-                case "Normal":
-                    normalCount++;
-                    break;
-                case "Pre-Hipertensi":
-                    preHypertensionCount++;
-                    break;
-                case "Hipertensi Stage 1":
-                    hypertension1Count++;
-                    break;
-                case "Hipertensi Stage 2":
-                    hypertension2Count++;
-                    break;
-            }
-        }
-
-        // Menampilkan hasil prediksi dalam persentase
-        int totalCount = bloodPressures.length;
-        DecimalFormat decimalFormat = new DecimalFormat("0.00");
-        System.out.println("\nHasil Prediksi Jangka Pendek:");
-        System.out.println("Normal: " + decimalFormat.format((double) normalCount / totalCount * 100) + "%");
-        System.out.println("Pre-Hipertensi: " + decimalFormat.format((double) preHypertensionCount / totalCount * 100) + "%");
-        System.out.println("Hipertensi Stage 1: " + decimalFormat.format((double) hypertension1Count / totalCount * 100) + "%");
-        System.out.println("Hipertensi Stage 2: " + decimalFormat.format((double) hypertension2Count / totalCount * 100) + "%");
-
-        // Menampilkan kemungkinan terbesar
-        System.out.println("\nKemungkinan terbesar:");
-        int maxCount = Math.max(Math.max(normalCount, preHypertensionCount), Math.max(hypertension1Count, hypertension2Count));
-        if (normalCount == maxCount) {
-            System.out.println("Normal");
-        }
-        if (preHypertensionCount == maxCount) {
-            System.out.println("Pre-Hipertensi");
-        }
-        if (hypertension1Count == maxCount) {
-            System.out.println("Hipertensi Stage 1");
-        }
-        if (hypertension2Count == maxCount) {
-            System.out.println("Hipertensi Stage 2");
-        }
-    }
-
-//     Fungsi untuk menghitung matriks transisi perubahan
-    public static int[][] calculateTransitionMatrix(int[] bloodPressures) {
-        int[][] transitionMatrix = new int[4][4];
-
-        for (int i = 0; i < bloodPressures.length - 1; i++) {
-            int currentState = getState(bloodPressures[i]);
-            int nextState = getState(bloodPressures[i + 1]);
-            transitionMatrix[currentState][nextState]++;
-        }
-
-        return transitionMatrix;
-    }
-
-
-
-    // Fungsi untuk menghitung matriks distribusi stabil
-    public static double[] calculateSteadyState(int[][] transitionMatrix) {
-        int n = transitionMatrix.length;
-        double[] steadyState = new double[n];
-        double[] initialProbabilities = new double[n];
-
-        for (int i = 0; i < n; i++) {
-            initialProbabilities[i] = 1.0 / n;
-        }
-
-        for (int i = 0; i < 100; i++) {
-            steadyState = multiplyMatrixAndVector(transitionMatrix, initialProbabilities);
-            initialProbabilities = steadyState.clone();
-        }
-
-        return steadyState;
-    }
-
-    // Fungsi untuk mengalikan matriks dan vektor
-    public static double[] multiplyMatrixAndVector(int[][] matrix, double[] vector) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        double[] result = new double[m];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                result[i] += matrix[i][j] * vector[j];
-            }
-        }
-
-        return result;
-    }
-
-    // Fungsi untuk mendapatkan kategori berdasarkan tekanan darah
-    public static int getState(int bloodPressure) {
-        if (bloodPressure < 120) {
-            return 0; // Normal
-        } else if (bloodPressure >= 120 && bloodPressure <= 139) {
-            return 1; // Pre-Hipertensi
-        } else if (bloodPressure >= 140 && bloodPressure <= 159) {
-            return 2; // Hipertensi Stage 1
-        } else {
-            return 3; // Hipertensi Stage 2
-        }
-    }
-
-    // Fungsi untuk menghitung prediksi berdasarkan tekanan darah dan matriks distribusi stabil
-    public static String calculatePrediction(int bloodPressure, double[] steadyState) {
-        int state = getState(bloodPressure);
-
-        if (steadyState[state] >= 0.5) {
-            return "Hipertensi Stage 2";
-        } else if (steadyState[state] >= 0.3) {
-            return "Hipertensi Stage 1";
-        } else if (steadyState[state] >= 0.1) {
-            return "Pre-Hipertensi";
-        } else {
-            return "Normal";
-        }
-    }
-
-    // Fungsi untuk menampilkan matriks
-    public static void displayMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    // Fungsi untuk menampilkan vektor
-    public static void displayVector(double[] vector) {
-        for (int i = 0; i < vector.length; i++) {
-            System.out.println(vector[i]);
+        // Menghitung prediksi jangka pendek atau jangka panjang
+        if (choice == 1) {
+            // Menghitung prediksi jangka panjang (30 hari)
+            System.out.println("\nPrediksi Jangka Panjang (30 hari):");
+            double[] prediksiJangkaPanjang = calculatePrediction(steadyState, probabilityMatrix, 30);
+            displayVectorPercentage(prediksiJangkaPanjang, kategori);
+            displayLargestProbability(prediksiJangkaPanjang, kategori);
+        } else if (choice == 2) {
+            // Menghitung prediksi jangka pendek (7 hari)
+            System.out.println("\nPrediksi Jangka Pendek (7 hari):");
+            double[] shortTermPrediction = calculatePrediction(steadyState, probabilityMatrix, 7);
+            displayVectorPercentage(shortTermPrediction, kategori);
+            displayLargestProbability(shortTermPrediction, kategori);
         }
     }
 }
-
--- stosk/stosk/src/Testing/markovChain.java
+++ stosk/stosk/src/Testing/markovChain.java
@@ -3,70 +3,93 @@ package Testing;
 import java.text.DecimalFormat;
 
 public class markovChain {
-    public static void main(String[] args) {
-        // Data tekanan darah awal
-        int initialBloodPressure = 130;
+    public static double[] calculateSteadyState(double[][] transitionMatrix) {
+        int n = transitionMatrix.length;
+        double[][] transposeMatrix = transposeMatrix(transitionMatrix);
 
-        // Matriks transisi perubahan tekanan darah
-        double[][] transitionMatrix = {
-                {0.5, 0.2, 0.2, 0.1},
-                {0.1, 0.4, 0.3, 0.2},
-                {0.2, 0.3, 0.3, 0.2},
-                {0.1, 0.2, 0.3, 0.4}
-        };
+        double[] eigenvalues = calculateEigenvalues(transposeMatrix);
+        double[][] eigenvectors = calculateEigenvectors(transposeMatrix, eigenvalues);
 
-        // Kategori tekanan darah
-        String[] hypertensionCategories = {"Normal", "Pre-Hipertensi", "Hipertensi Stage 1", "Hipertensi Stage 2"};
+        int dominantEigenvalueIndex = findDominantEigenvalueIndex(eigenvalues);
+        double[] steadyState = eigenvectors[dominantEigenvalueIndex];
 
-        // Menghitung perubahan tekanan darah menggunakan Markov Chain
-        double[] currentProbabilities = {0, 0, 0, 0};
-        currentProbabilities[getState(initialBloodPressure)] = 1;
+        normalizeVector(steadyState);
 
-        DecimalFormat decimalFormat = new DecimalFormat("0.00%");
-
-        System.out.println("Perubahan tekanan darah menggunakan Markov Chain:");
-        System.out.println("Tekanan awal: " + hypertensionCategories[getState(initialBloodPressure)]);
-
-        for (int i = 0; i < hypertensionCategories.length; i++) {
-            double probability = currentProbabilities[i];
-            System.out.print("Probabilitas menjadi " + hypertensionCategories[i] + ": " + decimalFormat.format(probability));
-
-            double[] nextProbabilities = new double[hypertensionCategories.length];
-            for (int j = 0; j < nextProbabilities.length; j++) {
-                for (int k = 0; k < currentProbabilities.length; k++) {
-                    nextProbabilities[j] += currentProbabilities[k] * transitionMatrix[k][j];
-                }
-            }
-
-            currentProbabilities = nextProbabilities;
-        }
-
-        // Menghitung persentase perubahan dari tekanan awal ke tekanan lain
-        double sum = 0;
-        for (double probability : currentProbabilities) {
-            sum += probability;
-        }
-
-        System.out.println("\n\nPersentase perubahan dari tekanan awal:");
-        for (int i = 0; i < hypertensionCategories.length; i++) {
-            double percentage = currentProbabilities[i] / sum;
-            System.out.print(hypertensionCategories[i] + ": " + decimalFormat.format(percentage));
-            if (i < hypertensionCategories.length - 1) {
-                System.out.print(" ");
-            }
-        }
+        return steadyState;
     }
 
-    // Fungsi untuk mendapatkan status berdasarkan tekanan darah
-    public static int getState(int bloodPressure) {
-        if (bloodPressure < 120) {
-            return 0; // Normal
-        } else if (bloodPressure >= 120 && bloodPressure <= 139) {
-            return 1; // Pre-Hipertensi
-        } else if (bloodPressure >= 140 && bloodPressure <= 159) {
-            return 2; // Hipertensi Stage 1
-        } else {
-            return 3; // Hipertensi Stage 2
+    private static double[][] transposeMatrix(double[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        double[][] transpose = new double[cols][rows];
+
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                transpose[i][j] = matrix[j][i];
+            }
+        }
+
+        return transpose;
+    }
+
+    private static double[] calculateEigenvalues(double[][] matrix) {
+        // TODO: Implement eigenvalue calculation algorithm
+        // You can use existing libraries or implement your own algorithm
+        // to calculate eigenvalues of the matrix.
+        // This implementation assumes the eigenvalues are already calculated.
+        // Replace this with your eigenvalue calculation implementation.
+        return new double[]{1.0, 0.5, 0.25}; // Placeholder values
+    }
+
+    private static double[][] calculateEigenvectors(double[][] matrix, double[] eigenvalues) {
+        // TODO: Implement eigenvector calculation algorithm
+        // You can use existing libraries or implement your own algorithm
+        // to calculate eigenvectors of the matrix.
+        // This implementation assumes the eigenvectors are already calculated.
+        // Replace this with your eigenvector calculation implementation.
+        return new double[][]{{1.0, 0.5, 0.25}, {0.5, 0.25, 0.125}, {0.25, 0.125, 0.0625}}; // Placeholder values
+    }
+
+    private static int findDominantEigenvalueIndex(double[] eigenvalues) {
+        int dominantIndex = 0;
+        double dominantValue = eigenvalues[0];
+
+        for (int i = 1; i < eigenvalues.length; i++) {
+            if (eigenvalues[i] > dominantValue) {
+                dominantValue = eigenvalues[i];
+                dominantIndex = i;
+            }
+        }
+
+        return dominantIndex;
+    }
+
+    private static void normalizeVector(double[] vector) {
+        double sum = 0.0;
+
+        for (double value : vector) {
+            sum += value;
+        }
+
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] /= sum;
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        double[][] transitionMatrix = {
+            {0.2, 0.5, 0.3},
+            {0.1, 0.6, 0.3},
+            {0.4, 0.3, 0.3}
+        };
+
+        double[] steadyState = calculateSteadyState(transitionMatrix);
+
+        System.out.println("Steady State:");
+        for (int i = 0; i < steadyState.length; i++) {
+            System.out.printf("State %d: %.4f\n", i, steadyState[i]);
         }
     }
 }
+
